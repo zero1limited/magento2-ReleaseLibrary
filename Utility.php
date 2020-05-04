398225c56c790ab164f17df2a67e44db1d74eb94
Arron Moss <arron.moss@zero1.co.uk>
2020-05-04 12:49:41 +0100

    Update Utility.php

--- Utility.php
+++ Utility.php
@@ -1,18 +1,44 @@
 <?php
 namespace Zero1\ReleaseLibrary;
 
+use Exception;
 use Magento\Variable\Model\VariableFactory;
+use Magento\Catalog\Api\CategoryManagementInterface;
+use Magento\Framework\Module\Dir;
+use Magento\Cms\Api\BlockRepositoryInterface;
+use Magento\Cms\Api\Data\BlockInterfaceFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Zero1\ReleaseLibrary\EntityAlreadyExistsException;
 
 class Utility
 {
     /** @var \Magento\Variable\Model\VariableFactory **/
     protected $customVariableFactory;
+    
+    /** @var CategoryManagementInterface */
+    protected $categoryManagement;  
+    
+    /** @var Dir\Reader */
+    private $reader;
+
+    /** @var BlockRepositoryInterface */
+    private $blockRepository;
+
+    /** @var BlockInterfaceFactory */
+    private $blockInterfaceFactory;
 
     public function __construct(
-        VariableFactory $customVariableFactory
+        VariableFactory $customVariableFactory,
+        CategoryManagementInterface $categoryManagement
+        Dir\Reader $reader,
+        BlockRepositoryInterface $blockRepository,
+        BlockInterfaceFactory $blockInterfaceFactory
     ){
         $this->customVariableFactory = $customVariableFactory;
+        $this->categoryManagement = $categoryManagement;
+        $this->reader = $reader;
+        $this->blockRepository = $blockRepository;
+        $this->blockInterfaceFactory = $blockInterfaceFactory;
     }
 
     public function createCustomVariable(
@@ -35,4 +61,69 @@ class Utility
             ->setPlainValue($plainValue)
             -> save();
     }
+    
+     /**
+     * Move Category
+     *
+     * @return CategoryTreeInterface
+     */
+    public function moveCategory($categoryId,$parentId,$afterId)
+    {
+        $isCategoryMoveSuccess = false;
+        try {
+            $isCategoryMoveSuccess = $this->categoryManagement->move($categoryId, $parentId, $afterId);
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+        }
+        return $isCategoryMoveSuccess;
+    }
+    
+    
+    private function getBlockSourceDirectory()
+    {
+        return sprintf(
+            '%s/%s',
+            $this->reader->getModuleDir(Dir::MODULE_VIEW_DIR, 'Zero1_ClientSetup'),
+            'block_source'
+        );
+    }
+    
+    private function createBlocksFromDir($source)
+    {
+        $blocks = array_diff(scandir($source), ['..', '.']);
+
+        foreach ($blocks as $block) {
+            $path = $directory . '/' . $block;
+            if (is_dir($path)) {
+                $this->createBlocksFromDirectory($path);
+                continue;
+            }
+
+            $id = pathinfo($block, PATHINFO_FILENAME);
+            $title = str_replace('-', ' ', $id);
+            $contents = file_get_contents($path);
+            $this->makeBlock($id, $title, $contents);
+        }
+    }
+    
+    private function makeBlock(
+        string $identifier,
+        string $title,
+        string $content,
+        array $stores = [0]
+    ) {
+        try {
+            $block = $this->blockRepository->getById($identifier);
+        } catch (NoSuchEntityException $exception) {
+            /** @var BlockInterface $block */
+            $block = $this->blockInterfaceFactory->create();
+            $block->->setTitle($title)
+                ->setIdentifier($identifier)
+                ->setIsActive(true)
+                ->setData('stores', $stores);
+        }
+
+        $block->setContent($content);
+        $this->blockRepository->save($block);
+    } 
 }
